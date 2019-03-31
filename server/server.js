da7f83b26bf41607e0b6e73113c55b8508a8aad7
shinyuy <shinyuy9@gmail.com>
2019-03-31 15:30:18 +0100

    Added brand and wood models for guitars, and admin middleware for authentication of admins

--- server/server.js
+++ server/server.js
@@ -15,11 +15,59 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Models
-const {User} = require('./models/user');
+const { User } = require('./models/user');
+const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
 
 // Middlewares
 const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
 
+  ////////////////////////////
+ //     WOODS              //
+////////////////////////////
+ 
+app.post('/api/product/wood', auth, admin, (req, res)=>{
+    const wood = new Wood(req.body);
+
+    wood.save((err, doc)=>{
+        if(err) return res.json({success: false, err});
+        res.status(200).send({
+            success: true,
+            wood: doc
+        })
+    })
+});
+
+app.get('/api/product/woods',(req, res)=>{
+    Wood.find({}, (err, woods)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(woods)
+    })
+})
+
+  ////////////////////////////
+ //     BRAND              //
+////////////////////////////
+
+app.post('/api/product/brand', auth, admin, (req, res)=>{
+     const brand = new Brand(req.body);
+
+     brand.save((err, doc)=>{
+         if(err) return res.json({success: false, err});
+         res.status(200).send({
+             success: true,
+             brand: doc
+         })
+     })
+})
+
+app.get('/api/product/brands', (req, res)=>{
+    Brand.find({}, (err, brands)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(brands)
+    })
+})
 
   ////////////////////////////
  //     USERS              //
