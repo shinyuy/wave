a881175d5a6ef371bb1d184d28db61e27b08c653
shinyuy <shinyuy9@gmail.com>
2019-04-01 16:34:31 +0100

    Added endpoint for getting single or multiple products based on product Id or other conditions

--- server/server.js
+++ server/server.js
@@ -18,10 +18,75 @@ app.use(cookieParser());
 const { User } = require('./models/user');
 const { Brand } = require('./models/brand');
 const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
 
 // Middlewares
 const { auth } = require('./middleware/auth');
 const { admin } = require('./middleware/admin');
+
+
+  ////////////////////////////
+ //     PRODUCTS           //
+////////////////////////////
+
+
+// BY Arrivals
+// /articles?sortBy=createdAt&order=desc&limits=4
+
+// By Sell
+// /articles?sortBy=sold&order=desc&limit=4
+app.get('/api/product/articles', (req, res)=>{
+
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.
+    find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy, order]]).
+    limit(limit). 
+    exec((err, articles)=>{
+        if(err) return res.status(400).send(err);
+        res.send(articles)
+    })
+
+})
+
+//  /api/product/article?id=kjfsdhjadj,dkjfdkj,adjfdkfkld,lsldjfndl?type=single
+app.get('/api/product/articles_by_id', (req, res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item=>{
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+
+    Product.find({'_id':{$in:items}}).
+    populate('brand').
+    populate('wood').
+    exec((err, docs)=>{
+        return res.status(200).send(docs)
+    })
+});
+
+
+app.post('/api/product/article', auth, admin, (req, res)=>{
+    const product = new Product(req.body);
+
+    product.save((err, doc)=>{
+        if(err) return res.json({success: false. err});
+        res.status(200).send({
+            success: true,
+            product: doc
+        })
+    })
+})
 
   ////////////////////////////
  //     WOODS              //
